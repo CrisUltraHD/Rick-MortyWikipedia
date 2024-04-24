9d66558d4e890b5577539e90253eaf6daa45b69a
Cristian Caride <cristiancaride@gmail.com>
2024-04-24 19:34:41 +0200

    Initial Commit

---
+++ Rick&MortyWikipedia/Rick_MortyWikipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyWikipediaApp.swift
+//  Rick&MortyWikipedia
+//
+//  Created by Cristian Caride on 24/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyWikipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
